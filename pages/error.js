6ff0ee1d09cdd8a7aa3045b540f797f23a8a21eb
amedama41 <kamo.tanabota@gmail.com>
2018-01-27 11:01:48 +0900

    Fix extracting error URL in error page

--- pages/error.js
+++ pages/error.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", (e) => {
     const params = new URLSearchParams(location.search.substr(1));
-    const errorURL = decodeURI(params.get("errorURL"));
+    const errorURL = params.get("errorURL");
     document.title = `Error: ${errorURL}`;
     const iframe = document.getElementById("errorURL");
     iframe.src = errorURL;
